8632677e450ea00d880fa7a7206b6532cb0be7f4
wings <han362123@naver.com>
2025-08-16 04:09:07 +0900

    Add files via upload

---
+++ static/lang/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+// 引入 多语言包
+import VueI18n from 'vue-i18n'
+import zh from './zh'
+import en from './en'
+
+
+Vue.use(VueI18n)
+
+// 创建实例
+const i18n = new VueI18n({
+    locale: "en",
+	// locale: uni.getStorageSync('lang') ? uni.getStorageSync('lang') : "zh",
+    messages: {
+        zh, en
+    }
+});
+
+export default i18n;
